91dabc73df639ff5cebecfe1b15845880bb4d795
Jeisiel10 <jeisiel10@hotmail.com>
2016-01-22 09:01:48 -0200

    alteracoes

--- Heranca/Modelos/Secretaria.cs
+++ Heranca/Modelos/Secretaria.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Heranca
+namespace Heranca.Modelos
 {
     class Secretaria : Funcionario
     {
--- Heranca/Testes/TestaFuncionarios.cs
+++ Heranca/Testes/TestaFuncionarios.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Heranca.Modelos;
 
-namespace Heranca
+namespace Heranca.Testes
+
 {
     class TestaFuncionarios
     {
